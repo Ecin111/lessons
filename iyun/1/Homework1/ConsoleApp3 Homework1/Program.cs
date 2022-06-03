1fbab2b488e46192df757612dbe11ca5727d84ea
Elcin Piriyev <elcinpiriyev111@gmail.com>
2022-06-03 21:24:19 +0400

    added file iyun 1

---
+++ iyun/1/Homework1/ConsoleApp3 Homework1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adinizi daxil edin");
+
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("soyadinizi daxil edin");
+
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("yasadiginiz seheri daxil edin");
+
+            string seher = Console.ReadLine();
+
+            Console.WriteLine(ad);
+            Console.WriteLine(soyad);
+            Console.WriteLine(seher);
+
+            Console.ReadLine();
+        }   
+
+    }
+}
---
+++ iyun/1/Homework3/Homework3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("seher1 adini daxil edin");
+
+            string seher1 = Console.ReadLine();
+
+            Console.WriteLine("seher2 adini daxil edin");
+
+            string seher2 = Console.ReadLine();
+
+            Console.WriteLine("seher3 adini daxil edin");
+
+            string seher3 = Console.ReadLine();
+
+            Console.WriteLine("seher4 adini daxil edin");
+
+            string seher4 = Console.ReadLine();
+
+            Console.WriteLine("seher5 adini daxil edin");
+
+            string seher5 = Console.ReadLine();
+
+            Console.WriteLine("seher6 adini daxil edin");
+
+            string seher6 = Console.ReadLine();
+
+            Console.WriteLine("seher7 adini daxil edin");
+
+            string seher7 = Console.ReadLine();
+
+            Console.WriteLine("seher8 adini daxil edin");
+
+            string seher8 = Console.ReadLine();
+
+            Console.WriteLine("seher9 adini daxil edin");
+
+            string seher9 = Console.ReadLine();
+
+            Console.WriteLine("seher10 adini daxil edin");
+
+            string seher10 = Console.ReadLine();
+
+
+            Console.WriteLine(seher1+","+ seher2+","+ seher3+","+ seher4+","+ seher5+","+ seher6+","+ seher7+","+ 
+                seher8+","+ seher9+","+ seher10 );
+
+
+            Console.ReadLine();
+           
+            
+            
+           
+            
+           
+           
+
+            Console.ReadLine();
+        }
+    }
+}
